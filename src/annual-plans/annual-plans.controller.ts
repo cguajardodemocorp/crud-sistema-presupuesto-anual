33a4fe0b8c0efee4ab7072b01cdac8c05a145bd9
Cristian Guajardo <cguajardo@democorp-cl>
2025-08-18 12:15:02 -0400

    add packages

--- src/annual-plans/annual-plans.controller.ts
+++ src/annual-plans/annual-plans.controller.ts
@@ -18,7 +18,7 @@ export class AnnualPlansController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: number) {
     return this.annualPlansService.findOne(+id);
   }
 
